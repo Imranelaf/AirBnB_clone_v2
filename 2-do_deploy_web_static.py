9ec4a9c958f5acd18e996f978a6d43543847fc96
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-10-05 15:32:38 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,34 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
+from fabric.api import put, run, local, env
+from os import path
 
-from fabric.api import env, put, run
-from os.path import exists
 
-env.hosts = ['100.25.180.140', '54.82.156.0']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ["54.205.197.191", "54.237.111.59"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
+    """Fabric script that distributes
+    an archive to your web server"""
 
-    if not exists(archive_path):
+    if not path.exists(archive_path):
         return False
-
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, '/tmp/')
-
-    archive_filename = archive_path.split('/')[-1]
-    folder_name = archive_filename.split('.')[0]
-    release_path = '/data/web_static/releases/{}'.format(folder_name)
-    run('mkdir -p {}'.format(release_path))
-    run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
-
-    # Delete the archive from the web server
-    run('rm /tmp/{}'.format(archive_filename))
-
-    run('mv {}/web_static/* {}/'.format(release_path, release_path))
-
-    # Remove the web_static directory
-    run('rm -rf {}/web_static'.format(release_path))
-
-    # Remove the symbolic link /data/web_static/current
-    run('rm -rf /data/web_static/current')
-
-    # Create a new symbolic link /data/web_static/current
-    run('ln -s {} /data/web_static/current'.format(release_path))
-
-    print('New version deployed!')
-    return True
+    try:
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
+        return True
+    except Exception as e:
+        return False
